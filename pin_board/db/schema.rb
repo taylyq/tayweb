3f7f10767cccb003d73ff02301b190baf0fa2658
tay <taylyq@yahoo.com>
2015-07-15 17:56:15 -0500

    pg database

--- pin_board/db/schema.rb
+++ pin_board/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150714231000) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pins", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
